e4c4a19c83b3ba210701d4bad4e9a245d5425842
Paul Taylor <pftaylor61@gmail.com>
2020-04-17 14:31:43 -0700

    Update 04172020 01

--- content.php
+++ content.php
@@ -37,15 +37,22 @@
 		<?php }
 		else { ?>
 			<div class="entry-content">
-				<?php the_content( wp_kses( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'qohelet' ), array( 'span' => array( 
+                                <?php 
+                                if ( is_home () || is_category() || is_archive() ) {
+                                    the_excerpt('');
+                                    } else {
+                                    the_content( wp_kses( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'qohelet' ), array( 'span' => array( 
 					'class' => array() ) ) )
 					); ?>
+
 				<?php wp_link_pages( array(
 					'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'qohelet' ),
 					'after' => '</div>',
 					'link_before' => '<span class="page-numbers">',
 					'link_after' => '</span>'
-				) ); ?>
+				) ); 
+                                } // end of if statements
+                                ?>
 			</div> <!-- /.entry-content -->
 		<?php } ?>
 
